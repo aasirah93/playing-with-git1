444c3fa56ba87d9d7446fe44461aca39893f71a3
Asifur Rahman <aasirah93@hotmail.co.uk>
2021-01-13 18:42:49 +0000

    JS Objects and Functions 1

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,3 +1,28 @@
-console.log('Hello World');
-console.warn('This is a warning');
-console.error('This is an error');
+// console.log('Hello World');
+// console.warn('This is a warning');
+// console.error('This is an error');
+
+// const age = 30;
+// age = 31;
+
+// console.log(age);
+
+// let score;
+
+// score = 10
+
+// console.log(score);
+
+const book1 = {
+    title: 'Book One',
+    author: 'John Doe',
+    year: '2013',
+    getSummary: function() {
+      return `${this.title} was written by ${this.author} in
+       ${this.year}`;
+    }
+};
+
+console.log(book1.getSummary());
+console.log(Object.values(book1));
+console.log(Object.keys(book1));
